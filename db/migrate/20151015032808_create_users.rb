5e5ed653333fced60c03e6e822907cb985cab856
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-15 00:03:46 -0400

    migration validations

--- db/migrate/20151015032808_create_users.rb
+++ db/migrate/20151015032808_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false, unique: true
       t.string :password_digest
 
       t.timestamps null: false
