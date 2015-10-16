a70bbe0ca6f26e0159c4c75430d556525165b19b
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-15 21:58:03 -0400

    add and style login page

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,33 @@
 class UserSessionsController < ApplicationController
   def new
+  	@user = User.new
+  	render 'new', layout: false
   end
 
   def create
+  	user = User.find_by(name: "Kelly")
+		if user && user.authenticate(user_session_params[:password])
+		  login_user(user)
+		end
+		redirect_to root_url
   end
 
   def destroy
+  	logout_user
+    redirect_to root_path
+  end
+
+  private
+
+  def user_session_params
+    params.require(:user_session).permit(:password)
   end
 end
+
+
+
+
+
+
+
+    
--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,2 +1,18 @@
 module UserSessionsHelper
+  def login_user(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logout_user
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
