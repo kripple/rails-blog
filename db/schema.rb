6b1b60e28aecaf445661043f4df40d696b48e489
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-15 23:15:30 -0400

    add about page and styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015033717) do
+ActiveRecord::Schema.define(version: 20151016022821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.string   "date",       null: false
-    t.text     "blurb",      null: false
-    t.text     "body",       null: false
-    t.boolean  "published",  null: false
-    t.string   "slug",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",       null: false
+    t.string   "date",        null: false
+    t.text     "description", null: false
+    t.text     "body",        null: false
+    t.boolean  "published",   null: false
+    t.string   "slug",        null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title",       null: false
+    t.string   "description", null: false
+    t.string   "url",         null: false
+    t.string   "source",      null: false
+    t.boolean  "published",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "taggings", force: :cascade do |t|
