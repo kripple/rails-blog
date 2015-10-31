61a1f0a0c429cb917f0a28ec4889838d8c155275
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-31 19:31:40 -0400

    add tags controller logic

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,23 @@
 class TagsController < ApplicationController
   def new
+  	redirect_unless_authorized
   	@tag = Tag.new
   end
 
   def create
+  	redirect_unless_authorized
+    @tag = Tag.create(tag_params)
+    if @tag.save
+      redirect_to :dashboard
+    else
+      flash[:errors] = @tag.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+	def tag_params
+    params.require(:tag).permit(:name)
   end
 end
