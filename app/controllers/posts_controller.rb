9923ffcfd1f17d0da20794a4fbadd699bf4931b3
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-31 15:55:40 -0400

    refactor post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,26 @@
 class PostsController < ApplicationController
+
+  before_action :find_post, only: [:show,:edit,:update]
+
   def index
     @posts = Post.all.where(published: true).order(created_at: :desc)
     @unpublished = Post.all.where(published: false).order(created_at: :desc) 
   end
 
   def show
-    p = Post.find_by_slug(params[:id])
-    p.published ? @post = p : @unpublished = p
-    redirect_to edit_post_path(p) if logged_in?
+    redirect_to edit_post_path(@post) if logged_in?
+    @unpublished = @post unless @post.published
   end
 
   def edit
-    @post = Post.find_by_slug(params[:id])
   end 
 
   def update
-    @post = Post.find_by(id: params[:post][:id])
     if @post.update_attributes(post_params) 
       redirect_to :dashboard 
     else
       flash[:errors] = @post.errors.full_messages
-      redirect_to post_path(@post)
+      redirect_to edit_post_path(@post)
     end
   end
 
@@ -47,5 +47,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :description, :date, :body, :published)
+  end
+
+  def find_post 
+    @post = Post.find_by_slug(params[:id])
   end
 end
