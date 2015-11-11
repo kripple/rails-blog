7de7c5f935d1b256e1a935b09f9d247e9a43f563
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-11-11 17:48:13 -0500

    debug js delete

--- app/assets/javascripts/taggables.js
+++ app/assets/javascripts/taggables.js
@@ -2,10 +2,11 @@ function initDeleteTaggables() {
 	$(".delete").on('click', function(e){
 		e.preventDefault();
 		e.stopImmediatePropagation(); // BLOGBLOGBLOG
+		var delete_url = $(this).attr('href')
 		if (confirm("Are you sure? This action is not reversible.")){
-			$.ajax({url: url, type: 'DELETE'}).done(function() {
-				var url = $(this).attr('href').match(/\/.+\//g)[0].slice(0,-1);
-	      window.location.replace(url); // BLOGBLOGBLOG
+			$.ajax({url: delete_url, type: 'DELETE'}).done(function() {
+				var redirect_url = delete_url.match(/\/.+\//g)[0].slice(0,-1);
+	      window.location.replace(redirect_url); // BLOGBLOGBLOG
 	    }.bind(this)).fail(function() { // BLOGBLOGBLOG
 	      console.log("ERROR: failed to delete");
 	    });
