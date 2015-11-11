343edd4cca692d3f416039953e155a19436f33ec
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-11-11 17:21:19 -0500

    cleanup comments

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,10 @@
 class ErrorsController < ActionController::Base
   def not_found
-  	# render layout: false
   end
 
   def internal_error
-  	# render layout: false
   end
 
   def unprocessable_entity
-  	# render layout: false
   end
 end
